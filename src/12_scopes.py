faf57ed29011fb18640562df8f7b63fe107e450c
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-06 17:28:19 -0400

    Completed 12_scopes program

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,11 +5,14 @@
 x = 12
 
 def change_x():
+    global x
     x = 99
+
 
 change_x()
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
+# Declare x as global in the function definition and set it to 99
 print(x)
 
 
@@ -19,12 +22,13 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
 
-    # This prints 120. What do we have to change in inner() to get it to print
-    # 999?
+    # This prints 120. What do we have to change in inner() to get it to print 999?
+    # Declare y as nonlocal inside inner()
     # Note: Google "python nested function scope".
     print(y)
 
