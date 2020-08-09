f9371ee37cc234a0b250d78b0cec407943ea82a5
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-09 19:34:17 -0400

    Completed js version of 02_datatypes

---
+++ src/js/02_datatypes.js
@@ -0,0 +1,11 @@
+
+let x = 5
+let y = "7"
+
+// Write a print statement that combines x + y into the integer value 12
+
+console.log(x + parseInt(y))
+
+// Write a print statement that combines x + y into the string value 57
+
+console.log(String(x) + y)
