b7b7f703259e176fc61859aa678b916b259ab828
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-09 20:16:13 -0400

    Added toFixed and toPrecision methods

--- src/js/04_printing.js
+++ src/js/04_printing.js
@@ -6,8 +6,8 @@ let z = "I like turtles!"
 
 
 // x is 10, y is 2.25, z is "I like turtles!"
-console.log('x is %d, y is %f, z is "%s"', x, y, z)
+console.log('x is %d, y is %f, z is "%s"', x, y.toFixed(2), z)
 
 
-console.log(`x is ${x}, y is ${y}, z is "${z}"`)
+console.log(`x is ${x}, y is ${y.toPrecision(3)}, z is "${z}"`)
 
