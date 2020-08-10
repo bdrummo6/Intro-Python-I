d1573cb987ec418b3bcac6c8a2ee32da4631b1ae
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-09 20:13:43 -0400

    Completed js version of 04_printing

---
+++ src/js/04_printing.js
@@ -0,0 +1,13 @@
+
+
+let x = 10
+let y = 2.24552
+let z = "I like turtles!"
+
+
+// x is 10, y is 2.25, z is "I like turtles!"
+console.log('x is %d, y is %f, z is "%s"', x, y, z)
+
+
+console.log(`x is ${x}, y is ${y}, z is "${z}"`)
+
