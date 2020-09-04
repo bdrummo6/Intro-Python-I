ef8506d4d795163b0a50180e174f94865a98ba4b
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-03 23:16:35 -0400

    Update 03_modules.py

--- src/03_modules.py
+++ src/03_modules.py
@@ -8,9 +8,11 @@ level operating system functionality.
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
+
 # Print out the command line arguments in sys.argv, one per line:
-for i in sys.argv:
-    print(i)
+for arg in sys.argv:
+    print(arg)
+
 
 # Print out the OS platform you're using:
 print(sys.platform)
